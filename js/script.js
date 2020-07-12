cf5b306870d1a48ac66671087afcb6baa4a2f3c8
james <hagenjh@gmail.com>
2020-07-12 02:42:29 -0600

    changes

--- js/script.js
+++ js/script.js
@@ -29,15 +29,91 @@ burger.addEventListener('click', () => {
   window.onscroll = function() {scrollFunction()};
 
   function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 10) {
       document.getElementById("nav").style.background = "#FCFCFC";
     } else {
       document.getElementById("nav").style.background = "transparent";
     }
   }
-  
+
   var rellax = new Rellax('.rellax');
 
 
+  var ctx = document.getElementById('myChart1').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'jibble'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                '#38ABA3',
+                '#AB5F38',
+                '#D1E8E7',
+                '#0C5E59',
+                '#F7996A',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+        
+            borderWidth: .5
+            
+        }]
+    },
+    options: {
+        
+    }
+});
 
-  
+var ctx = document.getElementById('myChart2').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'jibble'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                '#38ABA3',
+                '#AB5F38',
+                '#D1E8E7',
+                '#0C5E59',
+                '#F7996A',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+        
+            borderWidth: .5
+            
+        }]
+    },
+    options: {
+        
+    }
+});
+
+
+var ctx = document.getElementById('myChart3').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'jibble'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                '#38ABA3',
+                '#AB5F38',
+                '#D1E8E7',
+                '#0C5E59',
+                '#F7996A',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+        
+            borderWidth: .5
+            
+        }]
+    },
+    options: {
+        
+    }
+});
