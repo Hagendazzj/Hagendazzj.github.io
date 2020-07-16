616316745c9bd4d527d4ae7493d22e4ce2cb7e2b
james <hagenjh@gmail.com>
2020-07-16 10:42:41 -0600

    changes

--- js/script.js
+++ js/script.js
@@ -26,15 +26,7 @@ burger.addEventListener('click', () => {
   navSlide ();  
 
 
-  window.onscroll = function() {scrollFunction()};
 
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 10) {
-      document.getElementById("nav").style.background = "#FCFCFC";
-    } else {
-      document.getElementById("nav").style.background = "transparent";
-    }
-  }
 
   var rellax = new Rellax('.rellax');
 
@@ -43,24 +35,25 @@ burger.addEventListener('click', () => {
 var myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'jibble'],
+        labels: ['Less confusion working with others', 'Save time', 'less stress with being organized', 'Access from multiple devices'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [20, 35, 40, 5],
             backgroundColor: [
                 '#38ABA3',
-                '#AB5F38',
-                '#D1E8E7',
                 '#0C5E59',
+                '#D1E8E7',
+                '#AB5F38',
                 '#F7996A',
                 'rgba(255, 159, 64, 0.2)'
             ],
         
-            borderWidth: .5
+            borderWidth: 0
             
         }]
     },
     options: {
+      responsive: false
         
     }
 });
@@ -69,10 +62,10 @@ var ctx = document.getElementById('myChart2').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'jibble'],
+        labels: ['How it can be organized', 'How it looks', 'The networking capabilities', 'How it connects to what I use'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [40, 10, 10, 35],
             backgroundColor: [
                 '#38ABA3',
                 '#AB5F38',
@@ -82,12 +75,12 @@ var myChart = new Chart(ctx, {
                 'rgba(255, 159, 64, 0.2)'
             ],
         
-            borderWidth: .5
+            borderWidth: 0
             
         }]
     },
     options: {
-        
+      responsive: false
     }
 });
 
@@ -96,10 +89,10 @@ var ctx = document.getElementById('myChart3').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'jibble'],
+        labels: ['Easy to keep organized', 'Networking and sharing', 'I find new ideas', ' Easy to save backups'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [60, 25, 5, 5],
             backgroundColor: [
                 '#38ABA3',
                 '#AB5F38',
@@ -109,11 +102,32 @@ var myChart = new Chart(ctx, {
                 'rgba(255, 159, 64, 0.2)'
             ],
         
-            borderWidth: .5
+            borderWidth: 0
             
         }]
     },
     options: {
-        
+      
+      responsive: false
+
+       
     }
+  
 });
+
+Chart.defaults.global.defaultFontColor = '#fcfcfc';
+Chart.defaults.global.defaultFontSize = 12;
+Chart.defaults.global.defaultFontFmaily = 'Roboto';
+
+
+let chart = new Chart(ctx, {
+    type: 'doughnut',
+    data: data,
+    options: {
+            layout: {
+                legend: false
+            }
+        }
+    });
+
+ 
